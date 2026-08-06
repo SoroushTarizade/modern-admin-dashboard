0acda40b2613b8a7108806456500dc8cd6bb1e73
soroushtarizade <soroushtarizadeh7139@gmail.com>
2026-08-06 19:02:06 +0330

    end of responsive

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -1,34 +1,19 @@
 "use client";
 
 import { useState } from "react";
-
 import Sidebar from "@/components/sidebar/sideBar";
 import MobileSidebar from "@/components/sidebar/MobileSidebar";
-
 import Header from "@/components/header/Header";
 
-
 interface DashboardLayoutProps {
-
     children: React.ReactNode;
-
 }
 
-
-
 export default function DashboardLayout({
-
     children,
-
 }: DashboardLayoutProps) {
-
-
     const [openSidebar, setOpenSidebar] = useState(false);
-
-
-
     return (
-
         <div
             className="
                 min-h-screen
@@ -37,16 +22,11 @@ export default function DashboardLayout({
                 overflow-x-hidden
             "
         >
-
-
             {/* Desktop Sidebar */}
 
             <Sidebar />
 
-
-
             {/* Mobile Sidebar */}
-
             <MobileSidebar
 
                 open={openSidebar}
@@ -54,65 +34,37 @@ export default function DashboardLayout({
                 onClose={() => setOpenSidebar(false)}
 
             />
-
-
-
-
             {/* Main */}
 
             <div
-
                 className="
                     md:ml-[220px]
-
                     lg:ml-[240px]
-
                     min-h-screen
-
                     flex
-
                     flex-col
                 "
 
             >
-
-
                 <Header
 
                     onMenuClick={() =>
                         setOpenSidebar(true)
                     }
-
                 />
-
-
-
                 <main
 
                     className="
                         flex-1
-
-                        mt-20
-
+                        mt-10
                         p-4
-
                         sm:p-6
-
                         lg:p-8
                     "
-
                 >
-
                     {children}
-
-
                 </main>
-
-
-
             </div>
-
-
         </div>
 
     );
--- hooks/useDashboard.ts
+++ hooks/useDashboard.ts
@@ -46,7 +46,6 @@ export default function useDashboard() {
 
     return {
         loading,
-
         stats,
         sales,
         customers,
